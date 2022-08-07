bb83a3b951a8cc652d53a1b8b803e4d2322838db
guibrbs <200037994@aluno.unb.br>
2022-08-07 13:22:37 -0300

    feat: add colors and svgs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pattern-background': "url('assets/pattern_background.svg')"
+      }
+    },
+    colors: {
+      'background': '#181820',
+      'secondary': '#21212B',
+      'secondaryLight': '#292A37',
+      'text': '#F6F6F6',
+      'textDarker': '#B7B7B7',
+      'primary': '#00897B',
+      'primaryDarker': '#007165'
+    }
   },
   plugins: [],
 }
