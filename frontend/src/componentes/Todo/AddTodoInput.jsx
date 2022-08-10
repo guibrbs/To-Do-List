aa99570d4bba9376e1f07cf42c7bab2bdc697513
guibrbs <200037994@aluno.unb.br>
2022-08-10 13:13:13 -0300

    feat: render the todos on todolist

--- frontend/src/componentes/Todo/AddTodoInput.jsx
+++ frontend/src/componentes/Todo/AddTodoInput.jsx
@@ -1,6 +1,6 @@
-const AddTodoInput = ({ setTodoMessage, handleSubmitNewTodo, todoMessage }) => {
+const AddTodoInput = ({ setTodoMessage, handleSubmitNewTodo }) => {
   return (
-    <div className="w-full relative flex items-center">
+    <div className="w-full relative flex items-center mb-4">
       <span className="material-symbols-outlined text-lg text-textDarker absolute">
         add_task
       </span>
@@ -8,16 +8,21 @@ const AddTodoInput = ({ setTodoMessage, handleSubmitNewTodo, todoMessage }) => {
         className="w-full bg-secondary border-b border-b-textDarker focus:outline-none px-7
         placeholder:font-light text-text text-lg"
         placeholder="Insira sua nova tarefa"
-        onChange={(e) => setTodoMessage({
-          message: e.target.value, 
-          done: false
-        })}
+        onChange={(e) =>
+          setTodoMessage({
+            message: e.target.value,
+            done: false,
+          })
+        }
+        onKeyDown={(e) => e.key === "Enter" && (handleSubmitNewTodo())}
       />
-      <button className="absolute right-0" onClick={handleSubmitNewTodo}>
-        <span className="material-symbols-outlined text-lg text-textDarker hover:text-text 
-        transition-colors"
-        
-        >
+      <button
+        className="absolute right-0"
+        onClick={handleSubmitNewTodo}
+      >
+        <span
+          className="material-symbols-outlined text-lg text-textDarker hover:text-text 
+        transition-colors">
           send
         </span>
       </button>
--- frontend/src/componentes/Todo/TodoMap.jsx
+++ frontend/src/componentes/Todo/TodoMap.jsx
@@ -1,13 +1,42 @@
-const TodoMap = ({todos}) => {    
-    return (
-        <div className="border w-full h-full">
-            {todos.map((todo) => {
-                return (
-                    <p>{todo.message}</p>
-                )
-            })}
-        </div>
-    )
-}
+const TodoMap = ({ todos }) => {
+  return (
+    <div className="w-full h-full">
+      {todos.map((todo, index) => {
+        return (
+          <div
+            key={index}
+            className="text-text flex items-center w-full relative after:content-[''] after:absolute 
+            after:w-full after:h-[1px] after:bg-secondaryLight after:-bottom-4 mb-8"
+          >
+            <input
+              type="checkbox"
+              className="w-5 h-5 mr-4 text-pink appearance-none
+                         rounded-full checked:bg-pink checked:ring-text checked:ring-1 border-text
+                         border checked:border-secondary checked:border-2 cursor-pointer"
+              onClick={() => {}}
+            />
+            <p className={`font-light text-lg ${todo.done && "text-textDarker line-through"}`}>
+              {todo.message}
+            </p>
+            <div className="flex ml-auto gap-4">
+              <span
+                className="material-symbols-outlined text-xl cursor-pointer 
+              hover:brightness-90 transition-colors"
+              >
+                edit
+              </span>
+              <span
+                className="material-symbols-outlined text-xl cursor-pointer 
+              hover:brightness-90 transition-colors"
+              >
+                delete
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default TodoMap;
