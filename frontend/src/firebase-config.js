07925974ec6f7d330e2f5bf82879d70824918213
guibrbs <200037994@aluno.unb.br>
2022-08-11 10:21:06 -0300

    fix: format document with prettier

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,7 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
-import { addDoc, arrayRemove, arrayUnion, collection, deleteDoc, doc, getDocs, getFirestore, query, setDoc, updateDoc, where } from "firebase/firestore"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import {
+  addDoc,
+  arrayRemove,
+  arrayUnion,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCrRgWvvfEysKl41qoyt80nK8RobVn_IQk",
@@ -10,42 +27,39 @@ const firebaseConfig = {
   storageBucket: "to-do-list-v360.appspot.com",
   messagingSenderId: "855084215428",
   appId: "1:855084215428:web:67240bb98a149bdbdea6d1",
-  measurementId: "G-G0CMSP850N"
+  measurementId: "G-G0CMSP850N",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
+// eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(app);
 
 export const auth = getAuth();
 export const signUp = async (name, email, password) => {
-    try{
-        const userCredential = await createUserWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-        await addDoc(collection(db, "user"), {
-            name,
-            email,
-        });
-        return userCredential.user;
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await addDoc(collection(db, "user"), {
+      name,
+      email,
+    });
+    return userCredential.user;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const signIn = async (email, password) => {
-    try {
-      await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-    } catch (e) {
-      return({error: e.code});
-    }
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (e) {
+    return { error: e.code };
+  }
 };
 
 export const getUserInfo = async (userEmail) => {
@@ -54,85 +68,100 @@ export const getUserInfo = async (userEmail) => {
     const querySnapshot = await getDocs(q);
     const doc = querySnapshot.docs[0].data();
     const userDocID = querySnapshot.docs[0].id;
-    return {...doc, userDocID};
+    return { ...doc, userDocID };
   } catch (e) {
     console.log(e);
   }
 };
 
 export const createTodoList = async (listName, userDocID) => {
-  try{
-    await setDoc(doc(db, `user/${userDocID}/todosList`, listName),{
-      listName,
-      todos: []
-    }, { merge: true })
+  try {
+    await setDoc(
+      doc(db, `user/${userDocID}/todosList`, listName),
+      {
+        listName,
+        todos: [],
+      },
+      { merge: true }
+    );
     return true;
-  } catch (e){
+  } catch (e) {
     console.log(e);
   }
 };
 
 export const addNewTodo = async (userDocID, todoListName, message) => {
-  const todoListRef = doc(db, `user/${userDocID}/todosList/${todoListName}`); 
-  try{
+  const todoListRef = doc(db, `user/${userDocID}/todosList/${todoListName}`);
+  try {
     await updateDoc(todoListRef, {
-      todos: arrayUnion(message)
-    })
-  }catch (e){
+      todos: arrayUnion(message),
+    });
+  } catch (e) {
     console.log(e);
   }
-}
+};
 
 export const getTodoMessages = async (userDocID, todoListName) => {
-  try{
-    const q = query(collection(db, `user/${userDocID}/todosList`), where("listName", "==", todoListName));
+  try {
+    const q = query(
+      collection(db, `user/${userDocID}/todosList`),
+      where("listName", "==", todoListName)
+    );
     const querySnapshot = await getDocs(q);
     const doc = querySnapshot.docs[0].data();
     return doc;
-  } catch (e){
+  } catch (e) {
     console.log(e);
   }
-}
+};
 
 export const getAllTodoList = async (userDocID) => {
-  try{
+  try {
     const q = query(collection(db, `user/${userDocID}/todosList`));
     const querySnapshot = await getDocs(q);
     const docs = querySnapshot.docs.map((doc) => {
       return doc.data();
-    })
+    });
     return docs;
   } catch (e) {
     console.log(e);
   }
-}
+};
 
 export const deleteTodoList = async (userDocID, todoListName) => {
-  try{
-    await deleteDoc(doc(db, `user/${userDocID}/todosList/${todoListName}`))
-  } catch(e){
+  try {
+    await deleteDoc(doc(db, `user/${userDocID}/todosList/${todoListName}`));
+  } catch (e) {
     console.log(e);
   }
-}
+};
 
-export const deleteTodoFromTodoList = async (userDocID, todoListName, messageToDelete) => {
-  try{
-    const todoListRef = doc(db, `user/${userDocID}/todosList/${todoListName}`); 
+export const deleteTodoFromTodoList = async (
+  userDocID,
+  todoListName,
+  messageToDelete
+) => {
+  try {
+    const todoListRef = doc(db, `user/${userDocID}/todosList/${todoListName}`);
     await updateDoc(todoListRef, {
       todos: arrayRemove(messageToDelete),
-    }); 
-  } catch(e){
+    });
+  } catch (e) {
     console.log(e);
   }
-}
+};
 
-export const updateTodoItems = async (userDocID, todoListName, newTodoArray) => {
-  try{
-    const todoListRef = doc(db, `user/${userDocID}/todosList/${todoListName}`); 
+export const updateTodoItems = async (
+  userDocID,
+  todoListName,
+  newTodoArray
+) => {
+  try {
+    const todoListRef = doc(db, `user/${userDocID}/todosList/${todoListName}`);
     await updateDoc(todoListRef, {
       todos: newTodoArray,
-    }); 
-  } catch (e){
+    });
+  } catch (e) {
     console.log(e);
   }
-}
+};
